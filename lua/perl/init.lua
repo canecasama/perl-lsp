21746210f2726befc857dd6554ecc84de3cdf308
Caneca <caneca@gmail.com>
2024-06-27 10:07:31 +0200

    Add initial code

---
+++ lua/perl/init.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+M.setup = function()
+	require("perl.lsp").setup()
+end
+
+return M
---
+++ lua/perl/lsp.lua
@@ -0,0 +1,25 @@
+local M = {}
+
+local find_rust_bin = function()
+	return "/home/msoares/git_tree/perl-lsp-rust/target/debug/perl-nvim-lsp"
+end
+
+M.start = function()
+	vim.lsp.start({
+		name = "perl-nvim-lsp",
+		cmd = { find_rust_bin() },
+		root_dir = vim.fs.dirname(vim.fs.find({ "composer.json" }, { upward = true })[1]),
+	})
+end
+
+local group = vim.api.nvim_create_augroup("perl-nvim-lsp", {})
+M.setup = function()
+	vim.api.nvim_clear_autocmds({ group = group })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		pattern = { "perl" },
+		callback = M.start,
+	})
+end
+
+return M
